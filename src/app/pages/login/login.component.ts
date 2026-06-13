6744de1e6a5968d65484c129872114535d6ee0e8
Breno Alexandre <brenaomcerqueira@gmail.com>
2026-06-13 00:15:04 -0300

    Lógica alterada para se conectar com a API

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,24 +1,37 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms'; 
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  standalone: true, 
-  imports: [FormsModule], 
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css' 
+  styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  
+
   usuario = '';
   senha = '';
+  private router = inject(Router);
+  private http = inject(HttpClient);
 
   fazerLogin() {
-    if (this.usuario === 'admin' && this.senha === '123456') {
-      alert('Login efetuado com sucesso!');
-      
-    } else {
-      alert('Usuário ou senha incorretos.');
-    }
-  }
-}
+    const dadosLogin = {
+      nome: this.usuario,
+      senha: this.senha
+    };
+    this.http.post('http://localhost:3001/login', dadosLogin).subscribe({
+      next: (resposta: any) => {
+        this.router.navigate(['/home']);
+      },
+      error: (erro: any) => {
+        alert('Email ou senha incorretos. Por favor, tente novamente.');
+        this.usuario = '';
+        this.senha = '';
+      }
+    });
+
+
+  }}
