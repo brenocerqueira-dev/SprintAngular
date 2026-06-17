cadccbe65894cbf16cee378d91f49d46f128a7e7
Breno Alexandre <brenaomcerqueira@gmail.com>
2026-06-16 21:46:55 -0300

    Página Home Concluída com menu Hambúguer e botão logout

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  private roteador = inject(Router);
+  
+  // Variável que o HTML usa para saber se mostra o menu ou não
+  menuEstaAberto = false;
 
+  // A função que o botão de menu chama para abrir ou fechar o menu
+  abrirOuFecharMenu() {
+    this.menuEstaAberto = !this.menuEstaAberto;
+  }
+
+  // A função de LOGOUT 
+  sairDoSistema() {
+    this.roteador.navigate(['/login']);
+  }
 }
